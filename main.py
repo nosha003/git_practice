1ef5bede49f236602d42adcf834f0a9db8f0870b
Noshay <27n@mac124929.ornl.gov>
2022-06-14 14:33:41 -0400

    work in progress: it's busted

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 ##### This ia python script
+import sys
+
+
+def multiply_two_numbers(x, y):
+	return x * y
+
 
 if __name__ == "__main__":
-	print("Hellow world")
 
+	x = sys.argv[1]
+	y = sys.argv[2]
+	print("Hello world")
+	print("your numbers are", x, y)
 
+	z = mulitply_two_numbers(x,y)
+
+    #print(f"{x} + {y} = {z}")
+    print(x, "+", y, "=", z)
