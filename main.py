750c704230e99d463cc650905c59fb52b04a39b0
Noshay <27n@mac124929.ornl.gov>
2022-06-14 14:45:51 -0400

    it works!

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 ##### This ia python script
 import sys
-
-
 def multiply_two_numbers(x, y):
-	return x * y
+        return x * y
 
 
 if __name__ == "__main__":
+        x_str = sys.argv[1]
+        y_str = sys.argv[2]
 
-	x = sys.argv[1]
-	y = sys.argv[2]
-	print("Hello world")
-	print("your numbers are", x, y)
+        x = int(x_str)
+        y = int(y_str)
 
-	z = mulitply_two_numbers(x,y)
+        print("Hello world")
+        print("your numbers are", x, y)
+        
+        z = multiply_two_numbers(x,y)
 
-    #print(f"{x} + {y} = {z}")
-    print(x, "+", y, "=", z)
+        print(f"{x} + {y} = {z}")
